b99214f9a6282c18c9d1c5eeeaf41a7cac6f023d
Laila Campbell <lailacampbell96@gmail.com>
2020-03-24 04:41:11 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -18,6 +18,10 @@ class Person
 
 
 class PartnerError < StandardError
+  
+  def message
+    "You must give the get_married method an argument of an instance of the person class!"
+  end 
 end 
 end
 
